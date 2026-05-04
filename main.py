51815986b48cc653ab58c3c8ef59e15cd5330984
hackingerror404 <hackingerror404@gmail.com>
2026-05-04 17:03:56 -0600

    AWS S3 Bucket uploads working.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import boto3
 import ffmpeg
 import os
 
@@ -15,27 +16,37 @@ def encode_video(input_file, output_file, video_codec, audio_codec, crf, audio_b
         )
         ffmpeg.run(stream)
         print(f"Video converted successfully to {output_file}")
+        return True
     except ffmpeg.Error as e:
         print("Error converting video.")
         print(e.stderr.decode() if e.stderr else str(e))
+        return False
 
-def encoder_func(input_directory, output_directory, output_format, video_codec, audio_codec, crf, audio_bitrate):
+def scan_for_and_upload_videos(input_directory, output_directory, output_format, video_codec, audio_codec, crf, audio_bitrate, s3):
     # scan videos in directory
     for root, dirs, files in os.walk(input_directory):
         for f in files:
             input_file_path = os.path.join(root, f)
 
-            new_output = f[:f.index(".")] + "." + output_format
-            output_file_path = os.path.join(output_directory, new_output)
-
             if os.path.isfile(input_file_path):
-                encode_video(input_file_path, output_file_path, video_codec, audio_codec, crf, audio_bitrate)
+
+                new_output = f[:f.index(".")] + "." + output_format
+                aws_output = output_directory + new_output
+                
+                temp_output = f[:f.index(".")] + "_temp." + output_format
+                temp_file_path = os.path.join(input_directory, temp_output)
+
+                if encode_video(input_file_path, temp_file_path, video_codec, audio_codec, crf, audio_bitrate):
+                    s3.upload_file(temp_file_path, 'hackingerror404-bucket', aws_output)
+                    os.remove(temp_file_path)
 
 input_directory = "vidsInput"
-output_directory = "vidsOutput"
+output_directory = "vidsOutput/" # NEEDS TO END IN A '/' TO WORK.
 output_format = "mp4"
 video_codec = "libx264"
 crf = 23
 audio_bitrate = '128k'
 
-encoder_func(input_directory, output_directory, output_format, video_codec, "dummy", crf, audio_bitrate)
+s3 = boto3.client('s3')
+
+scan_for_and_upload_videos(input_directory, output_directory, output_format, video_codec, "dummy", crf, audio_bitrate, s3)
